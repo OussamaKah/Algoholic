17597a6e083c7dd092b1b389cb708e3255b880f0
okahoui <oussama.kahoui@capgemini.com>
2024-05-06 20:00:24 +0200

    backend added

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from core.config import settings
 app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
 
 
-@app.get("/")
+@app.get("/")git 
 def home():
     return {"message": "Hello Man"}
 
